18b8cd779b3c250986fbe6fbf18db2ba4c15866f
NishantTheProgrammer <knishant113@gmail.com>
2020-05-11 17:24:08 +0530

    Now values of matrix are horizontally valid... Add horizontal() funciton

--- index.php
+++ index.php
@@ -9,8 +9,90 @@
 <body>
     <table>
         <?php
-            $i = 0;
-            $j = 0;
+
+            $matrix= array(array());
+
+            // $matrix = [
+            //     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            //     [1, 2, 5, 6, 5, 6, 7, 8, 9],
+            //     [4, 4, 4, 4, 4, 4, 4, 4, 9],
+            //     [1, 2, 3, 4, 3, 6, 7, 8, 9],
+            //     [1, 2, 3, 6, 5, 6, 7, 8, 9],
+            //     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            //     [1, 2, 3, 3, 5, 6, 7, 6, 9],
+            //     [1, 3, 3, 4, 5, 6, 7, 8, 9],
+            //     [1, 2, 3, 4, 5, 3, 7, 8, 9]
+            // ];
+            for($i = 0; $i < 9; $i++)
+            {
+                for($j = 0; $j < 9; $j++)
+                {
+                    $matrix[$i][$j] = 0; // default value of every cell is 0
+                }
+            }
+
+            $possible = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+            for($i = 0; $i < 9; $i++)
+            {
+                for($j = 0; $j < 9; $j++)
+                {
+                    $valid = getValid($i, $j, $possible);
+                    
+                    echo "$i, $j  =>";
+                    foreach ($valid as $key) {
+                        echo $key;
+                    };
+                    foreach($valid as$element)
+                    {
+                        $matrix[$i][$j] = $valid[array_rand($valid)];                        
+                    }
+                    echo "<br>";
+                }
+                echo "<br>";                
+            }
+
+
+
+
+            function getValid($x, $y, $possible)
+            {
+                $valid = 0;
+                $valid = horizontal($x, $y, $possible);
+                $valid = vertical($x, $y, $valid);
+
+                return $valid;
+            }
+            function horizontal($x, $y, $possible)
+            {
+                global $matrix;
+                
+                $row = $matrix[$x];
+
+                foreach($row as $value)
+                {
+                    foreach($possible as $key=>$pos)
+                    {
+                        if($value == $pos)
+                        {
+                            unset($possible[$key]);  
+                        }
+                        
+                    }
+                }
+                 
+                return $possible;
+            }
+            function vertical($x, $y, $possible)
+            {              
+                return $possible;
+            }
+            function blockWise()
+            {
+            }
+
+
+
             for($i = 0; $i < 9; $i++)
             {
                 if($i == 2 || $i == 5)
@@ -22,14 +104,14 @@
                 }
                 for($j = 0; $j < 9; $j++)
                 {
-                    $num = rand(1, 9);
+                    
                     if($j == 2 or $j == 5)
                     {
-                        echo "<td class='vertical'>$num</td>";
+                        echo "<td class='vertical'>" . $matrix[$i][$j] ."</td>";
                     }
                     else
                     {
-                        echo "<td>$num</td>";
+                        echo "<td>" . $matrix[$i][$j] ."</td>";
                     }
                     
                 }
