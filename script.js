087ad3133d7d95dde2dbcd9762c1357d65f707c3
NishantTheProgrammer <knishant113@gmail.com>
2020-06-19 17:07:32 +0530

    horizontally, vertically and blockwise setisfying condition but can't print all 9 digits

--- script.js
+++ script.js
@@ -9,12 +9,113 @@ new Vue({
 
             [0, 0, 0,   0, 0, 0,   0, 0, 0],
             [0, 0, 0,   0, 0, 0,   0, 0, 0],
-            [0, 0, 0,   0, 0, 0,   8, 0, 0],
+            [0, 0, 0,   0, 0, 0,   0, 0, 0],
 
-            
+
             [0, 0, 0,   0, 0, 0,   0, 0, 0],
             [0, 0, 0,   0, 0, 0,   0, 0, 0],
             [0, 0, 0,   0, 0, 0,   0, 0, 0]
         ]
+    },
+    methods: {
+        populate(n)
+        {
+            for(let i = 0; i < 9; i++)
+            {
+
+                let cell = this.getBlock(i);
+                while(this.isEmpty(cell[0], cell[1]) ||
+                    this.vertically(cell[0], cell[1], n + 1) ||
+                    this.horizontally(cell[0], cell[1], n + 1)
+                )
+                {
+                    cell = this.getBlock(i);
+                }
+    
+
+
+
+                this.arr[cell[0]][cell[1]] = n + 1;
+            }
+            
+        },
+        isEmpty(x, y)
+        {
+            if(this.arr[x][y] == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        ,
+        vertically(x, y, n)
+        {
+            let row = this.arr[x];
+            if(row.includes(n))
+            {
+                console.log('row duplicate');
+                return true;
+            }
+            return false;
+        },
+        horizontally(x, y, n)
+        {
+            let col = [];
+
+            for(i = 0; i < 9; i++)
+            {
+                col.push(this.arr[i][y])
+            }
+            
+            if(col.includes(n))
+            {
+                console.log('col duplicate');
+                return true;
+            }
+            return false;
+        },
+        getBlock(n)
+        {
+            switch(n)
+            {
+                case 0:
+                    return [randInt(0, 3), randInt(0, 3)]
+                case 1:
+                    return [randInt(0, 3), randInt(3, 6)]
+                case 2:
+                    return [randInt(0, 3), randInt(6, 9)]
+
+
+                case 3:
+                    return [randInt(3, 6), randInt(0, 3)]
+                case 4:
+                    return [randInt(3, 6), randInt(3, 6)]
+                case 5:
+                    return [randInt(3, 6), randInt(6, 9)]
+
+
+                case 6:
+                    return [randInt(6, 9), randInt(0, 3)]
+                case 7:
+                    return [randInt(6, 9), randInt(3, 6)]
+                case 8:
+                    return [randInt(6, 9), randInt(6, 9)]
+            }
+        }
+    },
+    beforeMount() {
+        for(let i = 0; i < 1; i++)
+        {
+            this.populate(i);
+        }
     }
 });
+
+
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
